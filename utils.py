f912b7d442a37e9289a3fd4ec1c4ba6a15352f57
keyset <2129792+keyset@users.noreply.github.com>
2025-12-12 11:08:11 -0500

    add wait func

--- utils.py
+++ utils.py
@@ -9,3 +9,17 @@ def table_output(iterable, *fields):
         [[the_thing[field] for field in fields] for the_thing in iterable]
     )
     return snap_table
+
+
+def wait_for(wait_func, wait_for_value, wait_time, max_iters):
+    current_value = None
+    iters = 0
+    while current_value != wait_for_value:
+        new_value = wait_func()
+        iters += 1
+        if new_value != current_value:
+            print(f"{datetime.utcnow().isoformat()} currently {new_value}")
+            current_value = new_value
+        elif iters > max_iters:
+            break
+        sleep(wait_time)
